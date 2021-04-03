b7d5f44a8c1371caa3f961e60cfdc1d76f8fdcb1
SANKET MHATRE <sanket.mhatre@vit.edu.in>
2021-04-04 01:07:42 +0530

    updates

--- flask/app/views.py
+++ flask/app/views.py
@@ -5,7 +5,7 @@ import nltk.data
 from PIL import Image
 import pytesseract
 import json
-import re 
+import re
 from newspaper import Article
 from googlesearch import search
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
@@ -13,6 +13,8 @@ nltk.download('punkt')
 
 app.config["IMAGE_UPLOADS"] = r'D:\easy_solutions\flask\app\static\img\uploads'
 
+
+Questions = []
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -20,8 +22,22 @@ def index():
             image = request.files["image"]
             print(image)
             image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
-            args = { "image": image }
-            return get_image(args)
+            img = Image.open(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+            text = pytesseract.image_to_string(img, lang="eng")
+            tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+            tokenized_text = '\n \n'.join(tokenizer.tokenize(text))
+            sentences = tokenized_text.split('\n \n')
+            questions = []
+            for i in range(len(sentences)):
+                if(sentences[i].endswith('?') or  sentences[i].startswith("What") or sentences[i].startswith("When") or sentences[i].startswith("How") or sentences[i].startswith("Why") or sentences[i].startswith("Describe") or sentences[i].startswith("Explain")):
+                    questions.append(sentences[i])
+                    print(sentences[i])
+            
+            Questions = questions
+
+            return jsonify(questions)
+            # args = { "image": image }
+            # return get_image(args)
 
     return render_template("index.html")
 
@@ -53,33 +69,32 @@ class JSONEncoder(json.JSONEncoder):
                 return str(o)
             return json.JSONEncoder.default(self, o)
 
-questions = []
-@app.route("/get-image", methods=["GET", "POST"])
-def get_image(args):
-    print(request)
-    img = args["image"]
-    print(img.filename)
-    img = Image.open(os.path.join(app.config["IMAGE_UPLOADS"], img.filename))
-    text = pytesseract.image_to_string(img, lang="eng")
-    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-    tokenized_text = '\n \n'.join(tokenizer.tokenize(text))
-    sentences = tokenized_text.split('\n \n')
-    questions = []
-    for i in range(len(sentences)):
-        if(sentences[i].endswith('?') or  sentences[i].startswith("What") or sentences[i].startswith("When") or sentences[i].startswith("How") or sentences[i].startswith("Why") or sentences[i].startswith("Describe") or sentences[i].startswith("Explain")):
-            questions.append(sentences[i])
-            print(sentences[i])
+# questions = []
+# @app.route("/get-image", methods=["GET", "POST"])
+# def get_image(args):
+#     print(request)
+#     img = args["image"]
+#     print(img.filename)
+#     img = Image.open(os.path.join(app.config["IMAGE_UPLOADS"], img.filename))
+#     text = pytesseract.image_to_string(img, lang="eng")
+#     tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+#     tokenized_text = '\n \n'.join(tokenizer.tokenize(text))
+#     sentences = tokenized_text.split('\n \n')
+#     questions = []
+#     for i in range(len(sentences)):
+#         if(sentences[i].endswith('?') or  sentences[i].startswith("What") or sentences[i].startswith("When") or sentences[i].startswith("How") or sentences[i].startswith("Why") or sentences[i].startswith("Describe") or sentences[i].startswith("Explain")):
+#             questions.append(sentences[i])
+#             print(sentences[i])
 
-    # tosend = JSONEncoder().encode(questions)
-    return jsonify(questions)
-    # Questions = questions
-    
+#     return jsonify(questions)
+ 
 
-    return render_template("image.html", questions=questions)
+
+#     return render_template("image.html", questions=questions)
 
 
 @app.route('/answers')
-def answers():
+def answers(questions):
     search_querys = []
     for i in range(len(questions)):
         query = questions[i]
@@ -99,12 +114,12 @@ def answers():
             answers = 'Sorry! We were not able to find the answer'
         solution['answer'] = answers
         solutions.append(solution)
-   
+
     # for x,y in solutions.items():
     #         print('\n******************\n')
     #         print(x)
     #         print('\n answer = \n')
     #         print(y)
     #         print('\n******************\n')
-        
+
     return render_template('answers.html', solutions=solutions)
